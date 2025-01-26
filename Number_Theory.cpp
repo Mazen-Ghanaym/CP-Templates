37d2bbf4e5f543ada806358c869d54c0cf1faecb
Mazen-Ghanaym <mznghanayem@gmail.com>
2025-01-26 03:52:27 +0200

    Add another implementation for sieve

--- Number_Theory.cpp
+++ Number_Theory.cpp
@@ -264,6 +264,23 @@ struct Sieve
         }
     }
 
+    //! don't use to get prime numbers as i will not reach prime numbers greater than sqrt(n)
+    void sieve()
+    {
+        // Sieving till root
+        is_prime[0] = is_prime[1] = false;
+        for (int i = 2; i * i < N; i++)
+        {
+            if (is_prime[i])
+            {
+                for (int j = i * i; j < N; j += i)
+                {
+                    is_prime[j] = false;
+                }
+            }
+        }
+    }
+
     void linear_sieve()
     {
         is_prime[0] = is_prime[1] = false;
