385befa9e0a055fee4f191e7c5fcfafc9509879a
Mazen-Ghanaym <mznghanayem@gmail.com>
2025-02-06 23:12:31 +0200

    Add phi function

---
+++ Number_Theory/phi_function.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define int long long
+#define nl "\n"
+#define oo 1e9 + 1
+#define OO 1e18 + 1
+#define sp ' '
+#define sz(x) (int)(x.size())
+#define MOD 1000000007
+#define fixed(n) fixed << setprecision(n)
+#define sub_mod(a, b, m) ((((a) % m) - ((b) % m) + m) % m)
+#define add_mod(a, b, m) ((((a) % m) + ((b) % m)) % m)
+#define mult_mod(a, b, m) ((((a) % m) * ((b) % m)) % m)
+#define EPS 1e-9
+#define PI acos(-1)
+using namespace __gnu_pbds;
+using namespace std;
+
+int phi(int n) {
+    int result = n;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0)
+                n /= i;
+            result -= result / i;
+        }
+    }
+    if (n > 1)
+        result -= result / n;
+    return result;
+}
+
+const int N = 1e6 + 5;
+vector<int> phi(N);
+void precompute_phi() {
+    for (int i = 1; i < N; i++)
+        phi[i] = i;
+    for (int i = 2; i < N; i++) {
+        if (phi[i] == i) {
+            for (int j = i; j < N; j += i)
+                phi[j] -= phi[j] / i;
+        }
+    }
+}
+
+
+void solve(int tc)
+{
+    
+}
+signed main(void)
+{
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int tc = 1;
+    //cin >> tc;
+    int i = 1;
+    while (tc--)
+    {
+        // cout<<"Case #"<<i<<": ";
+        solve(i++);
+    }
+    return 0;
+}
