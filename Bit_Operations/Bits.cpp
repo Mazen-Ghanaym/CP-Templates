de717943823cd5fb8851edd60fc698174e3ba508
Mazen Ghanayem <mznghanayem@gmail.com>
2025-07-25 18:34:25 +0300

    Update Bits.cpp

--- Bit_Operations/Bits.cpp
+++ Bit_Operations/Bits.cpp
@@ -5,7 +5,5 @@
 #define check_bit(n, i) ((n) & (1 << (i)))
 #define count_bits(n) __builtin_popcount(n)
 #define count_bitsll(n) __builtin_popcountll(n)
-#define count_set_bits(n) __builtin_popcount(n)
-#define count_set_bitsll(n) __builtin_popcountll(n)
 #define count_zero_bits(n) (sizeof(n) * 8 - __builtin_popcount(n))
 #define count_zero_bitsll(n) (sizeof(n) * 8 - __builtin_popcountll(n))
