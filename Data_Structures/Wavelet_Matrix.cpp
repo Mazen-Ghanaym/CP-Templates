9301cecaf1997779deb39a5a6b994b1289f83f69
Mazen Ghanayem <mznghanayem@gmail.com>
2025-07-28 03:41:08 +0300

    Update Wavelet Matrix

--- Data_Structures/Wavelet_Matrix.cpp
+++ Data_Structures/Wavelet_Matrix.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 /*
     Wavelet Matrix
-    use for 2d points queries 
+    use for 2d points queries
     add point: O(log^2 n) -> x y w . add w to the point (x, y)
     ! sum in rectangle: O(log^2 n) -> x1 y1 x2 y2 -> sum in range [x1, x2) * [y1, y2)
     Memory: O(n log n)
@@ -17,7 +17,8 @@ using namespace std;
  * rank1(i): How many 1s are there in the sequence up to (but not including) position i
  * rank0(i): How many 0s are there in the sequence up to (but not including) position i
  */
-struct bit_vector {
+struct bit_vector
+{
     using u32 = uint32_t;
     using u64 = uint64_t;
 
@@ -29,18 +30,21 @@ struct bit_vector {
     bit_vector() = default;
     bit_vector(int _n) { init(_n); }
 
-    void init(int _n) {
+    void init(int _n)
+    {
         n = zeros = _n;
         block.assign(n / w + 1, 0);
         count.assign(block.size(), 0);
     }
 
+    inline u32 get(u32 i) const { return u32(block[i / w] >> (i % w)) & 1u; }
     inline void set(u32 i) { block[i / w] |= (1ULL << (i % w)); }
 
     // Enables POPCNT instruction for fast bit counting
-    __attribute__((target("popcnt"), optimize("O3,unroll-loops")))
-    void build() {
-        for (u32 i = 1; i < block.size(); ++i) {
+    __attribute__((target("popcnt"), optimize("O3,unroll-loops"))) void build()
+    {
+        for (u32 i = 1; i < block.size(); ++i)
+        {
             count[i] = count[i - 1] + _mm_popcnt_u64(block[i - 1]);
         }
         zeros = rank0(n);
@@ -49,12 +53,11 @@ struct bit_vector {
     inline u32 rank0(u32 i) const { return i - rank1(i); }
 
     // Enables BMI2 and POPCNT instructions for fast rank calculation
-    __attribute__((target("bmi2,popcnt"), optimize("O3,unroll-loops")))
-    inline u32 rank1(u32 i) const {
+    __attribute__((target("bmi2,popcnt"), optimize("O3,unroll-loops"))) inline u32 rank1(u32 i) const
+    {
         return count[i / w] + _mm_popcnt_u64(_bzhi_u64(block[i / w], i % w));
     }
 };
-
 
 /**
  * @brief A Wavelet Matrix that supports adding values to points and querying rectangle sums.
@@ -64,35 +67,44 @@ struct bit_vector {
  * @tparam T The type for weights/values (e.g., long long).
  */
 template <typename S, typename T>
-struct WaveletMatrix {
+struct WaveletMatrix
+{
     using u32 = uint32_t;
 
     // A standard 1D Fenwick Tree (or Binary Indexed Tree)
-    struct BIT {
+    struct BIT
+    {
         u32 N;
         vector<T> data;
 
         BIT() = default;
         BIT(int size) { init(size); }
 
-        void init(int size) {
+        void init(int size)
+        {
             N = size;
             data.assign(N + 1, 0);
         }
 
-        void add(u32 k, T x) {
-            for (++k; k <= N; k += k & -k) data[k] += x;
+        void add(u32 k, T x)
+        {
+            for (++k; k <= N; k += k & -k)
+                data[k] += x;
         }
 
-        T prefix_sum(u32 k) const {
+        T prefix_sum(u32 k) const
+        {
             T ret = T();
-            for (; k > 0; k -= k & -k) ret += data[k];
+            for (; k > 0; k -= k & -k)
+                ret += data[k];
             return ret;
         }
 
         // Sum over the half-open range [l, r)
-        T range_sum(u32 l, u32 r) const {
-            if (l >= r) return 0;
+        T range_sum(u32 l, u32 r) const
+        {
+            if (l >= r)
+                return 0;
             return prefix_sum(r) - prefix_sum(l);
         }
     };
@@ -107,78 +119,78 @@ struct WaveletMatrix {
     WaveletMatrix() = default;
 
     // Collects a potential point location. Must be called before build().
-    void add_point_location(S x, S y) {
+    void add_point_location(S x, S y)
+    {
         ps.emplace_back(x, y);
         ys.emplace_back(y);
     }
 
     // Builds the data structure from all collected point locations.
-    void build() {
+    void build()
+    {
         sort(ps.begin(), ps.end());
         ps.erase(unique(ps.begin(), ps.end()), ps.end());
         n = ps.size();
-
         sort(ys.begin(), ys.end());
         ys.erase(unique(ys.begin(), ys.end()), ys.end());
-
-        if (ys.empty()) return;
-
+        if (ys.empty())
+            return;
         vector<u32> cur(n), nxt(n);
-        for (int i = 0; i < n; ++i) cur[i] = get_y_id(ps[i].second);
-
-        lg = (ys.size() > 1) ? (log2(ys.size() - 1) + 1) : 1;
-
+        for (int i = 0; i < n; ++i)
+            cur[i] = get_y_id(ps[i].second);
+        lg = __lg(max(n, 1)) + 1;
         bv.assign(lg, n);
         bit.assign(lg, n);
-
-        for (int h = lg - 1; h >= 0; --h) {
-            for (int i = 0; i < n; ++i) {
-                if ((cur[i] >> h) & 1) bv[h].set(i);
+        for (int h = lg - 1; h >= 0; --h)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                if ((cur[i] >> h) & 1)
+                    bv[h].set(i);
             }
             bv[h].build();
-
             // Stably partition `cur` into `nxt` based on the h-th bit
-            auto it0 = nxt.begin();
-            auto it1 = nxt.begin() + bv[h].zeros;
-            for (int i = 0; i < n; ++i) {
-                if ((cur[i] >> h) & 1) *it1++ = cur[i];
-                else *it0++ = cur[i];
-            }
+            array<decltype(begin(nxt)), 2> it{begin(nxt), begin(nxt) + bv[h].zeros};
+            for (int i = 0; i < n; ++i)
+                *it[bv[h].get(i)]++ = cur[i];
             swap(cur, nxt);
         }
     }
 
     // Returns the compressed ID for an x-coordinate.
-    int get_x_id(S x) const {
-        auto it = lower_bound(ps.begin(), ps.end(), make_pair(x, numeric_limits<S>::min()));
+    int get_x_id(S x) const
+    {
+        auto it = lower_bound(ps.begin(), ps.end(), make_pair(x, S()), [](const P &a, const P &b)
+                              { return a.first < b.first; });
         return distance(ps.begin(), it);
     }
 
     // Returns the compressed ID for a y-coordinate.
-    int get_y_id(S y) const {
+    int get_y_id(S y) const
+    {
         auto it = lower_bound(ys.begin(), ys.end(), y);
         return distance(ys.begin(), it);
     }
 
     // Adds a value to a point.
-    void add(S x, S y, T val) {
+    void add(S x, S y, T val)
+    {
         auto it = lower_bound(ps.begin(), ps.end(), make_pair(x, y));
-        if (it == ps.end() || *it != make_pair(x, y)) return;
+        if (it == ps.end() || *it != make_pair(x, y))
+            return;
         int i = distance(ps.begin(), it);
 
-        u32 y_val = get_y_id(y);
-        for (int h = lg - 1; h >= 0; --h) {
-            bool go_right = (y_val >> h) & 1;
-            
-            if (go_right) {
-                i = bv[h].rank1(i) + bv[h].zeros;
-            } else {
-                i = bv[h].rank0(i);
-            }
+        for (int h = lg - 1; h >= 0; --h)
+        {
+            int i0 = bv[h].rank0(i);
+            if (bv[h].get(i))
+                i += bv[h].zeros - i0;
+            else
+                i = i0;
             bit[h].add(i, val);
         }
     }
-    
+
     // Internal helper to sum points with x-id in [l, r) and y-id < upper_y_id.
     T sum_internal(int l, int r, u32 upper_y_id) const {
         if (l >= r) return 0;
