0a6bc5cff0ffda75f2ee46d854bcf2286c75a7db
Mazen Ghanayem <mznghanayem@gmail.com>
2025-08-04 01:45:33 +0300

    Update Trie

--- Data_Structures/Trie.cpp
+++ Data_Structures/Trie.cpp
@@ -19,78 +19,111 @@
 #define PI acos(-1)
 using namespace __gnu_pbds;
 using namespace std;
-struct Trie {
-    struct Node {
- 
-        Node* child[26];
+struct Trie
+{
+    struct Node
+    {
+
+        Node *child[26];
         bool is_word;
         int freq;
- 
-        Node(){
+
+        Node()
+        {
             memset(child, 0, sizeof(child));
             is_word = false;
             freq = 0;
         }
     };
 
-    Node* root;
+    Node *root;
     char DEFAULT;
 
-    Trie(){
+    Trie()
+    {
         root = new Node;
         DEFAULT = 'a';
     }
-    
-    void insert(const string& word){
-        Node* curr = root; 
-        for(auto& c : word){
-            if(!curr -> child[c - DEFAULT]) 
-                curr -> child[c - DEFAULT] = new Node;
-            curr = curr -> child[c - DEFAULT];
-            curr -> freq++;
+
+    void insert(const string &word)
+    {
+        Node *curr = root;
+        for (auto &c : word)
+        {
+            if (!curr->child[c - DEFAULT])
+                curr->child[c - DEFAULT] = new Node;
+            curr = curr->child[c - DEFAULT];
+            curr->freq++;
         }
-        curr -> is_word = true;
+        curr->is_word = true;
     }
-  
-    void erase(const string& word, int idx, Node* curr){
-        if(idx == sz(word)) return void(curr -> is_word = curr -> freq > 1);
-        erase(word, idx + 1, curr -> child[word[idx] - DEFAULT]);
-        if(--curr -> child[word[idx] - DEFAULT] -> freq == 0){
-            delete curr -> child[word[idx] - DEFAULT];
-            curr -> child[word[idx] - DEFAULT] = nullptr;
+
+    void erase(const string &word, int idx, Node *curr)
+    {
+        if (idx == sz(word))
+            return void(curr->is_word = curr->freq > 1);
+        erase(word, idx + 1, curr->child[word[idx] - DEFAULT]);
+        if (--curr->child[word[idx] - DEFAULT]->freq == 0)
+        {
+            delete curr->child[word[idx] - DEFAULT];
+            curr->child[word[idx] - DEFAULT] = nullptr;
         }
     }
 
-    bool search(const string& word){
-        Node* curr = root; 
-        for(auto& c : word){
-            if(!curr -> child[c - DEFAULT]) return false;
-            curr = curr -> child[c - DEFAULT];
+    bool search(const string &word)
+    {
+        Node *curr = root;
+        for (auto &c : word)
+        {
+            if (!curr->child[c - DEFAULT])
+                return false;
+            curr = curr->child[c - DEFAULT];
         }
-        return curr -> is_word;
+        return curr->is_word;
     }
- 
-    void erase(const string& word){
-        if(search(word)) 
+
+    void erase(const string &word)
+    {
+        if (search(word))
             erase(word, 0, root);
     }
 
-    bool is_prefix(const string& word){
-        Node* curr = root; 
-        for(auto& c : word){
-            if(!curr -> child[c - DEFAULT]) return false;
-            curr = curr -> child[c - DEFAULT];
+    bool is_prefix(const string &word)
+    {
+        Node *curr = root;
+        for (auto &c : word)
+        {
+            if (!curr->child[c - DEFAULT])
+                return false;
+            curr = curr->child[c - DEFAULT];
         }
         return true;
     }
 
-    int count(const string& word){
-        Node* curr = root; 
-        for(auto& c : word){
-            if(!curr -> child[c - DEFAULT]) return 0;
-            curr = curr -> child[c - DEFAULT];
+    int count(const string &word)
+    {
+        Node *curr = root;
+        for (auto &c : word)
+        {
+            if (!curr->child[c - DEFAULT])
+                return 0;
+            curr = curr->child[c - DEFAULT];
         }
-        return curr -> freq;
+        return curr->freq;
+    }
+    ~Trie()
+    {
+        function<void(Node *)> deleteNode = [&](Node *node)
+        {
+            if (!node)
+                return;
+            for (int i = 0; i < 26; ++i)
+            {
+                deleteNode(node->child[i]);
+            }
+            delete node;
+        };
+        deleteNode(root);
     }
 };
 void solve(int tc)
@@ -115,7 +148,7 @@ signed main(void)
 {
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int tc = 1;
-    //cin >> tc;
+    // cin >> tc;
     int i = 1;
     while (tc--)
     {
