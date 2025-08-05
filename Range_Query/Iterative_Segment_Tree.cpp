4f8befc2e55fff53022c8762098efde627221069
Mazen Ghanayem <mznghanayem@gmail.com>
2025-08-05 04:21:44 +0300

    Update Format

--- Range_Query/Iterative_Segment_Tree.cpp
+++ Range_Query/Iterative_Segment_Tree.cpp
@@ -5,54 +5,67 @@
 using namespace std;
 // iterative segment tree
 // zero-based indexing
-const int N = 1e5;  // limit for array size
-int n;  // array size
+const int N = 1e5; // limit for array size
+int n;             // array size
 int t[2 * N];
 
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+void build()
+{ // build the tree
+  for (int i = n - 1; i > 0; --i)
+    t[i] = t[i << 1] + t[i << 1 | 1];
 }
 
-void modify(int p, int value) {  // set value at position p
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+void modify(int p, int value)
+{ // set value at position p
+  for (t[p += n] = value; p > 1; p >>= 1)
+    t[p >> 1] = t[p] + t[p ^ 1];
 }
 
-int query(int l, int r) {  //! sum on interval [l, r)
+int query(int l, int r)
+{ //! sum on interval [l, r)
   int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
+  for (l += n, r += n; l < r; l >>= 1, r >>= 1)
+  {
+    if (l & 1)
+      res += t[l++];
+    if (r & 1)
+      res += t[--r];
   }
   return res;
 }
 
 void solve()
 {
-    cin >> n;
-    int q;
-    cin >> q;
-    for (int i = 0; i < n; i++) {
-        cin >> t[n + i];
+  cin >> n;
+  int q;
+  cin >> q;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> t[n + i];
+  }
+  build();
+  while (q--)
+  {
+    int type, x, y;
+    cin >> type >> x >> y;
+    if (type == 1)
+    { // update
+      modify(x, y);
     }
-    build();
-    while (q--) {
-        int type, x, y;
-        cin >> type >> x >> y;
-        if (type == 1) {  // update
-            modify(x, y);
-        } else {  // query
-            cout << query(x, y) << '\n';
-        }
+    else
+    { // query
+      cout << query(x, y) << '\n';
     }
+  }
 }
 signed main(void)
 {
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int tc = 1;
-    //cin >> tc;
-    while (tc--)
-    {
-        solve();
-    }
-    return 0;
+  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  int tc = 1;
+  // cin >> tc;
+  while (tc--)
+  {
+    solve();
+  }
+  return 0;
 }
