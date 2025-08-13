a912afdb42bf22a6bb6a0511d44f227cc116cea7
Mazen-Ghanaym <mznghanayem@gmail.com>
2025-08-14 01:03:38 +0300

    Update Misc.cpp

--- Miscellaneous/Misc.cpp
+++ Miscellaneous/Misc.cpp
@@ -31,15 +31,25 @@ mt19937 seed{random_device{}()};
 uniform_int_distribution<int> rnd(INT_MIN, INT_MAX);
 inline int random() { return rnd(seed); }
 inline int random(int l, int r) { return uniform_int_distribution<int>(l, r)(seed); }
-void solve(int tc) {}
-signed main(void) {
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    int tc = 1;
-    // cin >> tc;
-    int i = 1;
-    while (tc--) {
-        // cout<<"Case #"<<i<<": ";
-        solve(i++);
-    }
-    return 0;
-}
+
+
+// GP Hash Table
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+struct custom_hash {
+  static uint64_t splitmix64(uint64_t x) {
+    x += 0x9e3779b97f4a7c15;
+    x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    return x ^ (x >> 31);
+  }
+  size_t operator()(uint64_t x) const {
+    static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+    return splitmix64(x + FIXED_RANDOM);
+  }
+};
+
+gp_hash_table<int, int, custom_hash> mp;
+
